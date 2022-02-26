4d03c85649156664c360c4ccc4a782b88a55e2ca
Alan Jian <alanjian85@outlook.com>
2022-02-26 23:35:18 +0800

    Hello window

--- learnbgfx/src/main.cc
+++ learnbgfx/src/main.cc
@@ -1,5 +1,86 @@
+#include <iostream>
+
+#include <bgfx/bgfx.h>
+#include <bgfx/platform.h>
+#include <bx/bx.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_syswm.h>
+
+void windowResizeCallback(SDL_Window* window, int width, int height) {
+    bgfx::reset(width, height);
+    bgfx::setViewRect(0, 0, 0, width, height);
+}
 
 int main() {
-    
+    SDL_Init(0);
+    SDL_Window* window = SDL_CreateWindow(
+        "LearnBgfx", 
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        800, 600, 
+        SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE
+    );
+    if (window == nullptr) {
+        std::cerr << "Failed to create SDL2 window" << std::endl;
+        SDL_Quit();
+        return -1;
+    }
+
+    bgfx::renderFrame();
+
+    SDL_SysWMinfo wmi;
+    SDL_VERSION(&wmi.version);
+    SDL_GetWindowWMInfo(window, &wmi);
+
+    bgfx::Init init;
+#if BX_PLATFORM_LINUX || BX_PLATFORM_BSD
+    init.platformData.ndt = wmi.info.x11.display;
+    init.platformData.nwh = reinterpret_cast<void*>(wmi.info.x11.window);
+#elif BX_PLATFORM_OSX
+    init.platformData.nwh = wmi.info.cocoa.window;
+#elif BX_PLATFORM_WINDOWS
+    init.platformData.nwh = wmi.info.win.window;
+#endif
+    init.resolution.width = 800;
+    init.resolution.height = 600;
+    init.resolution.reset = BGFX_RESET_VSYNC;
+    if (!bgfx::init(init)) {
+        std::cerr << "Failed to initialize BGFX" << std::endl;
+        return -1;
+    }
+
+    bgfx::setViewRect(0, 0, 0, 800, 600);
+
+    bool quit = false;
+    while (!quit) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    quit = true;
+                    break;
+                case SDL_WINDOWEVENT:
+                    switch (event.window.event) {
+                        case SDL_WINDOWEVENT_RESIZED:
+                            windowResizeCallback(window, event.window.data1, event.window.data2);
+                            break;
+                    }
+                    break;
+                case SDL_KEYDOWN:
+                    switch (event.key.keysym.sym) {
+                        case SDLK_ESCAPE:
+                            quit = true;
+                            break;
+                    }
+            }
+
+            bgfx::setViewClear(0, BGFX_CLEAR_COLOR, 0x334d4d);
+            bgfx::touch(0);
+
+            bgfx::frame();
+        }
+    }
+
+    bgfx::shutdown();
+    SDL_Quit();
+    return 0;
 }
